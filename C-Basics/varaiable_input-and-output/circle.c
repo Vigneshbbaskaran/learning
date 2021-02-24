19679f6d6537afcfed59aa13c40d4643b188dd27
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-02-24 12:50:35 +0000

    Upload New File

---
+++ C-Basics/varaiable_input-and-output/circle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	float r;
+	printf("enter rad value\n");
+	scanf("%f", &r);
+	printf("area of circle =%f \nperimeter of circle =%f\n",3.14*r*r,2*3.14*r  );
+return 0;
+}
