cdf38df98b44428408f92a9e324c74f887a623bb
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-02-24 12:21:45 +0000

    Upload New File

---
+++ C-Basics/Day-2-input-output/2_input-output.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+float a,b;
+int div;
+printf("Enter two number to perform division\n");
+scanf("%f%f",&a,&b);
+div=a/b;
+printf("quotient =%d\n", div);
+
+
+return 0;
+}
