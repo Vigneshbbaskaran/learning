2612162b80608349a0b07efcf76053539136e13e
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-02-24 15:02:23 +0000

    Upload New File

---
+++ C-Basics/Arithmetic-operators/1.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+int main()
+{
+	int temp,num;
+	printf("enter 4 digit number\n");            //getting numbers
+	scanf("%d", &num);
+	
+	temp = num %10;   
+	printf("op1:\n%d\n", temp);                 //op1:     
+	temp = num %100;  
+	temp = temp /10;   
+        printf("%d\n", temp);
+	temp = num %1000;
+	temp = temp /100;
+        printf("%d\n", temp);
+	temp = num %10000;
+	temp = temp / 1000;
+        printf("%d\n", temp);
+
+	temp = num /1000;
+        printf("op2:\n%d\n", temp);                 //op2:     
+        temp = num %1000;  
+        temp = temp /100;   
+        printf("%d\n", temp);
+        temp = num %100;
+        temp = temp /10;
+        printf("%d\n", temp);
+        temp = num %10;
+        printf("%d\n", temp);
+
+        temp = num /1000;
+        printf("op3:\n%d\n", temp);                 //op3:     
+        temp = num /100;
+        printf("%d\n", temp);
+        temp = num /10;
+        printf("%d\n", temp);
+        printf("%d\n", num);
+
+	printf("op4:\n%d\n", num);
+	temp = num /10;
+	printf("%d\n", temp);
+	temp = num /100;
+        printf("%d\n", temp);
+        temp = num /1000;
+        printf("%d\n", temp);
+
+	
+return 0;
+}
