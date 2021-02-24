105bc1f9908c6c6df2bfe46b4610397f9dc5e4d6
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-02-24 12:45:13 +0000

    Upload New File

---
+++ C-Basics/varaiable_input-and-output/2_input-output.c
@@ -0,0 +1,16 @@
+//2)try input and output on different variables,create variables and then formatted output and inputs, print some expressions with arithmetic operations 
+
+
+#include<stdio.h>
+int main()
+{
+float a,b;
+int div;
+printf("Enter two number to perform division\n");
+scanf("%f%f",&a,&b);
+div=a/b;
+printf("quotient =%d\n", div);
+
+
+return 0;
+}
