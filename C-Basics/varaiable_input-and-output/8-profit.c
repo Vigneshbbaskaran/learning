19f325eeb26ba33ebf22479c02c6dc934ea634b5
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-02-24 12:59:17 +0000

    Update 8-profit.c

--- C-Basics/varaiable_input-and-output/8-profit.c
+++ C-Basics/varaiable_input-and-output/8-profit.c
@@ -6,14 +6,14 @@ int main()
 	printf("Enter number of items:\n");
 	scanf("%d",&n);
 	printf("enter the sales price and profit\n");
-	for(i=0;i<n;i++)
-	{
-		printf("sales =");
-		scanf("%f",&sp);
-		printf("profit =");
-                scanf("%f",&profit);
-		cp = sp - profit;
-                printf("cost price for entered item =%f\n", cp);
-	}
+	printf("sales =");
+	scanf("%f",&sp);
+	printf("profit =");
+    scanf("%f",&profit);
+    sp= sp/n
+    profit = profit/n
+	cp = sp - profit;
+    printf("cost price for entered item =%f\n", cp);
+	
 	return 0;
 }
