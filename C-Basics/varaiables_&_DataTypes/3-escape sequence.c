99d014b79a204b77c927952e68dd66209bf2d4b3
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-02-25 17:51:03 +0000

    Update 3-escape sequence.c

--- C-Basics/varaiables_&_DataTypes/3-escape sequence.c
+++ C-Basics/varaiables_&_DataTypes/3-escape sequence.c
@@ -4,7 +4,7 @@ int main()
 	int a;
 	printf("enter 4 digit number\n");
 	scanf("%d",&a);
-	printf("%d\b\b\b   \n",a);
+	printf("%d\b\b\b   \n",a);   // each back space moves the cursor one left(didnt edit any content) and replaced by the white space
 	printf("%d\b\b  \n",a);
 	printf("%d\b \n",a);
 	printf("%d\n",a);
