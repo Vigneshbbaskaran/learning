c7921c103578be3bfa0412b520ab350c9cb77320
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-02-25 12:01:43 +0000

    Upload New File

---
+++ C-Basics/varaiables_&_DataTypes/1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char a; //character to ASCII part
+	printf("enter a value for :Character to ACII\n");
+	scanf("%c",&a);
+        printf("ASCII value:%d\n",a);
+
+
+	int b; //ASCII to character part
+	printf("enter a value for :ASCII to character\n");
+	scanf("%d",&b);
+	printf("Character:%c\n", b);
+	return 0;
+}
+
