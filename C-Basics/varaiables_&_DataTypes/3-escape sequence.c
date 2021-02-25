7ba664207b68300b12ffba0ea56b6d437a5ba08d
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-02-25 17:27:38 +0000

    Replace 3-escape sequence.c

--- C-Basics/varaiables_&_DataTypes/3-escape sequence.c
+++ C-Basics/varaiables_&_DataTypes/3-escape sequence.c
@@ -4,10 +4,10 @@ int main()
 	int a;
 	printf("enter 4 digit number\n");
 	scanf("%d",&a);
-	printf("%d\b\b\b ",a);
-	printf("%d\b\b ",a);
-	printf("%d\b ",a);
-	printf("%d ",a);
+	printf("%d\b\b\b   \n",a);
+	printf("%d\b\b  \n",a);
+	printf("%d\b \n",a);
+	printf("%d\n",a);
 	return 0;
 }
 
