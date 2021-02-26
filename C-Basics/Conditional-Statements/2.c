609c98db7a45e682caba8fb9227878a362f99180
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-02-26 14:32:29 +0000

    Upload New File

---
+++ C-Basics/Conditional-Statements/2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a, temp;
+printf("enter the number\n");
+scanf("%d",&a);
+
+temp= a%2;
+
+if(temp==0)
+printf("%d is a even number\n",a);
+else
+printf("%d is a odd number\n",a);
+
+return 0;
+}
