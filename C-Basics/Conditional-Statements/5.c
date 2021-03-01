a85ec17085b60d34794319254b66c5cfa7f19ba8
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-01 13:11:02 +0000

    Upload New File

---
+++ C-Basics/Conditional-Statements/5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main()
+{
+int a,b,c,d,temp;
+printf("Enter four values for comparision\n ");
+scanf("%d %d %d %d", &a, &b, &c, &d);
+
+if (a > b)
+{
+    if (a > c) 
+    {
+      if (a > d)
+        temp = a;
+      else
+        temp = d;
+    } else
+    {
+      if (c > d)
+        temp = c;
+      else
+        temp = d;
+    }
+
+}
+else
+{
+    if (b > c) 
+    {
+      if (b > d)
+        temp = b;
+      else
+        temp = d;
+    }
+    else 
+    {
+      if (c > d)
+        temp = c;
+      else
+        temp = d;
+    }
+  }
+
+printf("\n %d is the bigest number", temp);
+
+  return 0;
+}
