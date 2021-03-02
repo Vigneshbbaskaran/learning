158722a09b9c404fef905474fdf0cdbdb77f8406
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-02 14:30:02 +0000

    Upload New File

---
+++ C-Basics/Conditional Operator assignments/9.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter a year to check for leap year:\n");
+	scanf("%d",&a);
+
+	a%4==0 ? printf("LEAP YEAR\n") : printf("NOT A LEAP YEAR\n");
+
+	return 0;
+}
