8eb7b6659ab1b5e0b97e68676c748e9d5eeb4f27
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-03 12:32:11 +0000

    Replace 9_leap-year.c

--- C-Basics/Conditional Operator assignments/9_leap-year.c
+++ C-Basics/Conditional Operator assignments/9_leap-year.c
@@ -5,7 +5,8 @@ int main()
 	printf("enter a year to check for leap year:\n");
 	scanf("%d",&a);
 
-	((y%4==0) && (y%100 !=0)) || (y%400==0) ? printf("LEAP YEAR\n") : printf("NOT A LEAP YEAR\n");
+	((a%4==0) && (a%100 !=0)) || (a%400==0) ? printf("LEAP YEAR\n") : printf("NOT A LEAP YEAR\n");
 
 	return 0;
 }
+
