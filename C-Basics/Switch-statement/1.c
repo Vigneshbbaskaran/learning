16335951cd3728adbe84e9894c7085117dff11c0
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-03 13:01:27 +0000

    Upload New File

---
+++ C-Basics/Switch-statement/1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+char a;
+printf("enter a character \n");
+scanf("%c",&a);
+
+switch (a)
+{
+case 'a': 
+case 'e':
+case 'i':
+case 'o':
+case 'u':
+case 'A':
+case 'E':
+case 'I':
+case 'O':
+case 'U':
+	printf("enter character is vowel\n");
+	break;
+default:
+	printf("entered characer is not vowel\n");
+}
+return 0;
+}
