1e177bba30f4d67845802a2c5a5307447968dbaa
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-04 10:11:27 +0000

    Update 5_big-2var.c

--- C-Basics/Switch-statement/5_big-2var.c
+++ C-Basics/Switch-statement/5_big-2var.c
@@ -4,13 +4,16 @@ int main()
 int a,b;
 printf("enter a and b :\n");
 scanf("%d%d",&a,&b);
-switch(a>=b)
+switch(a>b? 1 : (a<b ? 0 : 2 ))
 {
 case 1:
 	printf("%d is bigger\n",a);
 	break;
 case 0:
 	printf("%d is bigger\n",b);
+	break;
+case 2:
+	printf("Both number are same\n");
 	break;
 }
 
