202bc43674cf5b50b9df27ff0464f9bdbfcc60f3
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-15 18:26:32 +0000

    Update 7_n-positive-numbers.c

--- C-Basics/LOOPS/7_n-positive-numbers.c
+++ C-Basics/LOOPS/7_n-positive-numbers.c
@@ -1,3 +1,8 @@
+// 7)WaP to read few numbers and keep reading until exactly n positive numbers are entered by the user, and print the sum of positive numbers.
+// eg., n = 5
+// input : 1  -2  -3   4  -5  -3   7   8  -1  -4  -5   9
+// output: 29
+
 #include<stdio.h>
 int main()
 {
