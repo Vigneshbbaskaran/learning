fb93caa52c778bf0cfb073872cdb328a3d0567c1
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-15 19:55:49 +0000

    Update 10_mul-using-loop.c

--- C-Basics/LOOPS/10_mul-using-loop.c
+++ C-Basics/LOOPS/10_mul-using-loop.c
@@ -1,5 +1,6 @@
 //10)WaP to calculate the product of 2 numbers without using multipliction operator (*).
 //eg., input : 4 5
+//output : 20 (should be calculated in a repetitive addition method)
 
 #include<stdio.h>
 int main()
