53d3065e2ac70bb6586618a89d0b0cfcefdfc5f6
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-17 17:06:46 +0000

    Upload New File

---
+++ C-Basics/March16/9.c
@@ -0,0 +1,52 @@
+
+
+#include<stdio.h>
+int main()
+{
+	int a,i,j,temp=0;
+	printf("enter number:\n");
+	scanf("%d",&a);
+
+	for(i=0;i<a;i++)  //for loop
+	{
+		printf("\n");
+		for(j=0;j<=i;j++)
+		{
+			printf("%d ", ++temp);
+		}
+	}
+	printf("\n\n");
+//-----------------------------------------------------------------------------------------------------	
+	i=temp=0;
+	while(i<a)       //while loop
+	{
+	j=0;
+		printf("\n");
+		while(j<=i)
+		{
+			printf("%d ",++temp);
+			j++;
+		}
+	i++;
+	}
+	printf("\n\n");
+//----------------------------------------------------------------------------------------------
+
+        i=temp=0;
+        do       // Do while loop
+        {
+        j=0;
+                printf("\n");
+                do
+                {
+                        printf("%d ",++temp);
+                        j++;
+                } while(j<=i);
+        i++;
+	}while(i<a);
+        printf("\n");
+
+
+	return 0;
+}
+
