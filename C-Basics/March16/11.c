b85d710af35303c12a1b468d851a6000ba909fef
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-17 18:36:30 +0000

    Update 11.c

--- C-Basics/March16/11.c
+++ C-Basics/March16/11.c
@@ -1,3 +1,12 @@
+/*
+11)Write a program to input a number and count the digits in it.
+Eg. 
+0 - number of digits 1
+12 – number of digits 2
+155 – number of digits 3
+*/
+
+
 #include<stdio.h>
 int main()
 {
