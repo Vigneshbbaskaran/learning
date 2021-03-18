c42a115f7d7da0c6ab31115a6d01002053e467c9
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-18 07:40:52 +0000

    Replace 1.c

--- C-Basics/Function/1.c
+++ C-Basics/Function/1.c
@@ -1,23 +1,28 @@
-/*1)Modify the calculator program - with functions ,Add(),Sub(),Mul(),Div(),Mod(). Each function should take the inputs as parameters and return the result as output.
-Hint: int add(int , int); 
-int sub (int, int);
-*/
 #include<stdio.h>
+
+int add(int a,int b);
+int sub(int a,int b);
+int mul(int a,int b);
+float div(int a,int b);
+int mod(int a,int b);
+
 int main()
+
 {
 	int a,b;
-	float c;
 	printf("enter two numbers for perform various operation:\n");
 	scanf("%d%d",&a,&b);
+	
 
-	printf("Add =%d\n",add(a,b) );
+	printf("\nAdd =%d\n",add(a,b) );
 	printf("Sub =%d\n",sub(a,b) );
 	printf("Mul =%d\n",mul(a,b) );
-	printf("Div =%d\n",div(a,b) );
+	printf("Div =%.2f\n",div(a,b) );
 	printf("Mod =%d\n",mod(a,b) );
 	
 	return 0;
 }
+
 
 int add(int a,int b)
 	{
@@ -34,9 +39,11 @@ int mul(int a,int b)
 	return a*b;
 	}
 
-int div(int a,int b)
+float div(int a,int b)
 	{
-		return a/b;
+		float c;
+		c= a/(float)b;
+		return c;
 	}
 
 int mod(int a, int b)
