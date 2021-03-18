2621604358baac80b07e84849f4a97d1f5e10bfb
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-18 05:12:48 +0000

    Upload New File

---
+++ C-Basics/LOOPS-1/12.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,i=1,count=0,temp=0,remain;
+	printf("enter 2 number\n");
+	scanf("%d%d",&a,&b);
+	temp=a;
+	if(a>=b)
+	{
+		while(i)
+		{	temp= temp-b;
+			remain=temp;
+			count++;
+			if(temp<b)
+				i=0;
+		}
+	}
+	else
+		remain=a;
+	printf("Quotient = %d\nremainder = %d\n", count, remain);
+	return 0;
+}
+
+
