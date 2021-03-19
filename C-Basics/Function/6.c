161bb5441374ff391700d917fcf3857976752db3
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-19 02:05:02 +0000

    Upload New File

---
+++ C-Basics/Function/6.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int fun_alpha_num(char c);
+
+int main()
+{
+	int i;
+	char c;
+	printf("enter a char\n");
+	c = getchar();
+
+	i=fun_alpha_num(c);
+	i==1 ? printf("alphanumeric\n") : printf("Not alphanumeric\n"); 
+	return 0;
+}
+
+int fun_alpha_num(char c)
+{
+	int i=0;
+	if( (c>=65 &&  c<=90) || (c>=97 && c<=122) || (c>=48 && c<=57) )
+	       i=1;
+	return i;
+}
