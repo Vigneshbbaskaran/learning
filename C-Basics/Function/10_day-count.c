7bec6070a60c98777ecd7dc8c29e974feb93ee6c
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-19 05:59:52 +0000

    Update 10_day-count.c

--- C-Basics/Function/10_day-count.c
+++ C-Basics/Function/10_day-count.c
@@ -1,3 +1,5 @@
+//10)Write a function that accepts a month and year as input, and returns the number of days of the month as output. Print the number of days in a given month and year.
+
 #include<stdio.h>
 int day(int,int);
 
