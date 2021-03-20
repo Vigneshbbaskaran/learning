3c93764739431f37ab80c7f49f2a9da047beee5c
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-20 05:52:48 +0000

    Upload New File

---
+++ LABTest/20MAR202/compare.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int DateCompare(int d1,int m1, int y1,int d2, int m2,int y2)
+{
+
+
+
+	if(y1>y2)
+		return 1;
+	else if(y1==y2) // incase both year same, check further detail
+	{
+		if(m1>m2)
+			return 1;
+		else if(m1==m2) // incase both month same, check further detail
+		{
+			if(d1>d2)
+				return 1;
+			else if(d1==d2)
+				return 1;
+			else
+				return 0;
+		}
+		else
+			return 0;
+	}
+	else
+		return 0;
+
+
+
+}
+
