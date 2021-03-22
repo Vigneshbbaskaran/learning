3c29604a30a5ac4dcf03c3da79453883e870c52d
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-22 18:05:32 +0000

    Update main.c

--- C-Basics/storage-classes/SuperMarket/main.c
+++ C-Basics/storage-classes/SuperMarket/main.c
@@ -1,3 +1,13 @@
+/*4)WAP - for the 4 departments of a super market : Sales, Purchase, Stock and SuperMarket in four different source code files.
+Stock : Maintains a variable for available stock.
+Sales : Has one function: Sell – It will reduce the stock by the number of items sold. Before selling check if enough stock is available.
+Purchase : Has one function : Purchase – will increase the stock by the number of items purchased.Set a max value for stock, before purchasing check if the stock room has enough space for new items or not.
+SuperMarket :
+Display menu with choices : 
+1 ) check AvailableStock 
+2 ) Purchase new items
+3) Sell items … this should be continuous as long as the user wants to continue. Program should stop when the user wants to quit."
+*/
 #include<stdio.h>
 void sales(int);
 void purchase(int);
