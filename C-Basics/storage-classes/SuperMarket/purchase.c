e1947bac7d3331f0e4a253d47166fadee708e961
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-22 18:02:34 +0000

    Upload New File

---
+++ C-Basics/storage-classes/SuperMarket/purchase.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int stocks;
+void addstocks(int);
+void purchase(int amount)
+{
+int capacity=1000;
+	if (capacity >=(stocks+amount))
+	{
+		addstocks(amount);
+		printf("\n%d Stocks purchased successfully",amount);
+		printf("\nstocks available = %d",stocks);
+	}
+	else
+	{
+		printf("\nNot sufficient Storage");
+	}
+return;
+}
+
