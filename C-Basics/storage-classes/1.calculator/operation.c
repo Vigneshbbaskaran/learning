4d1fb0a6d534ad96e569bc9f245e8fdc7f26ad85
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-22 19:41:35 +0000

    Upload New File

---
+++ C-Basics/storage-classes/1.calculator/operation.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+extern int a,b;
+int add(void)
+	{
+	return a+b;
+	}
+
+int sub(void)
+	{
+	return a-b;
+	}	
+
+int mul(void)
+	{
+	return a*b;
+	}
+
+float div(void)
+	{
+		float c;
+		c= a/(float)b;
+		return c;
+	}
+
+int mod(void)
+	{
+		return a%b;
+	}
+	
+int operation(char c)
+{
+switch(c)
+{
+case '+':
+	return add() ;
+case '-': 
+	return sub(); 
+case '*': 
+	return mul();
+case '/': 
+	return div();
+case '%': 
+	return mod();
+	
+}
+}
+
+	
