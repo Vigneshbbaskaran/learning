2d826ae89ebffe38145617bffb91d707409316cc
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-22 19:54:18 +0000

    Update main.c

--- C-Basics/storage-classes/2.calculator/main.c
+++ C-Basics/storage-classes/2.calculator/main.c
@@ -1,4 +1,9 @@
+/*Rewrite the calculator program so that the main function is in main.c , 
+remaining functions are in another source code file ,operations.c. 
 
+2 ) Create the operand variables as global variables and use them in the operation functions directly, 
+without passing as arguments. Function return value should be stored in another global variable in operations file and be used to print in main.
+*/
 #include<stdio.h>
 int a,b,result;
 int add(void);
