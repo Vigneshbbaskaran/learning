79b516f65e759b033ee4151ca5f47561b4101005
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-23 17:40:47 +0000

    Update main.c

--- C-Basics/SuperMarket_version-2/2.SuperMarket/main.c
+++ C-Basics/SuperMarket_version-2/2.SuperMarket/main.c
@@ -1,3 +1,9 @@
+/*2) When the user chooses Sell the stock option, and there is not enough stock, then instead of cancelling the sales, 2 options will be displayed:
+i) Sell the existing stock
+ii) Cancel the sales
+
+The user can choose any one of the above options and close the transaction.
+*/
 #include<stdio.h>
 void sales(int);
 void purchase(int);
