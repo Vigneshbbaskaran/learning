09c68efa0a2e54b93dfca462092d93266f4ccbbc
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-25 17:52:06 +0000

    Update 5_var-array-size.c

--- C-Basics/ARRAYS/5_var-array-size.c
+++ C-Basics/ARRAYS/5_var-array-size.c
@@ -1,3 +1,11 @@
+/*
+5)	"Write the following program.
+Declare an integer variable ‘size’. 
+Take the value of ‘size’ from user using scanf.
+Declare an array of any datatype, and give the integer variable ‘size’, in place of size of the array  and initialize it to some values.(syntax: float array[size] = {1.0,2.5};)
+Using for loop, try to take user input using scanf. Loop should execute ‘size’ number of times.
+Using for loop, print the elements of the array."
+*/
 #include<stdio.h>
 int main()
 {
