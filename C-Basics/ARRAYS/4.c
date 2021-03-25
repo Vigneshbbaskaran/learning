52d487efae8cb3b00d3e2ab95407e73796c15456
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-25 17:27:46 +0000

    Upload New File

---
+++ C-Basics/ARRAYS/4.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+int main()
+{
+/*
+        int i,a[5]={};
+        for(i=0;i<5;i++)
+        {
+                printf("a[%d] = %d\nsize = %d\n",i,a[i],sizeof(a[i]));
+        }
+*/
+/*
+        int i,a[10]={2.4,5.6,7.3};
+        for(i=0;i<10;i++)
+        {
+                printf("\na[%d] = %d\n\tsize = %d\n",i,a[i],sizeof(a[i]));
+        }
+*/
+
+/*
+        int i,a[3]={1,2,3,4,5};
+        for(i=0;i<10;i++)
+        {
+                printf("\na[%d] = %d\n\tsize = %d\n",i,a[i],sizeof(a[i]));
+        }
+*/
+     
+	
+  
+/*
+        int i,a[0]={};
+        for(i=0;i<10;i++)
+        {
+                printf("\na[%d] = %d\n\tsize = %d\n",i,a[i],sizeof(a[i]));
+        }
+*/
+	
+/*
+        int i,a[0]={1,2,3,4,5};
+        for(i=0;i<10;i++)
+        {
+                printf("\na[%d] = %d\n\tsize = %d\n",i,a[i],sizeof(a[i]));
+        }
+*/
+/*
+        int i,a[]={};
+        for(i=0;i<10;i++)
+        {
+                printf("\na[%d] = %d\n\tsize = %d\n",i,a[i],sizeof(a[i]));
+        }
+*/
+///*
+        int i,a['a'];
+        for(i=0;i<10;i++)
+        {
+                printf("\na[%d] = %d\n\tsize = %d\n",i,a[i],sizeof(a[i]));
+        }
+//*/
+
+  
+
+	return 0;
+}
+
