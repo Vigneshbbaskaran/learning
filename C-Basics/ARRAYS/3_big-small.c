840a37f72b0a72f3feee80e35061af34ab404490
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-25 16:59:13 +0000

    Replace 3_big-small.c

--- C-Basics/ARRAYS/3_big-small.c
+++ C-Basics/ARRAYS/3_big-small.c
@@ -1,10 +1,7 @@
-/*3)	Declare an array of size 10. Enter 10 elements and save them in the array. Print the array .
-Find out the biggest and smallest numbers and their indexes and print. 
-*/
 #include<stdio.h>
 int main()
 {
-	int i,temp1[1],temp2[1],a[10];
+	int i,temp1[2],temp2[2],a[10];
 	for(i=0;i<10;i++)
 	{
 		printf("\na[%d] =",i);
@@ -21,20 +18,20 @@ int main()
 	temp2[0]=a[0];
 	for(i=0;i<10;i++)
 	{
-		if(a[i]>temp1[0])
+		if(a[i]>=temp1[0])
 		{
 			temp1[0]=a[i]; // values
 			temp1[1]=i;   // index
 		}
-		if(a[i]<temp2[0])
+		if(a[i]<=temp2[0])
 		{
 			temp2[0] =a[i]; // values
 			temp2[1] =i;  // index
-			
+		
 
 		}
 	}
-	
+
 	printf("\nbiggest among all \na[%d] = %d\n",temp1[1],temp1[0]);
         printf("\nsmallest among all \na[%d] = %d\n",temp2[1],temp2[0]);	
 	
