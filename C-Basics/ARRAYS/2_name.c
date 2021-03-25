85dd17b7527d8cdd68535918bb79ae7023de7602
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-25 15:51:41 +0000

    Update 2.c

---
+++ C-Basics/ARRAYS/2_name.c
@@ -0,0 +1,21 @@
+/*
+2)	"Write the following program :
+Declare a character array without size.
+Initialize the array with characters of your name.
+Print your name using for loop."
+*/
+#include<stdio.h>
+int main()
+{
+	char c[]={'v','i','g','n','e','s','h'};
+	int i;
+	for(i=0;i<7;i++)
+	{
+		
+	putchar(c[i]);
+		
+
+	}
+	printf("\n");
+	return 0;
+}
