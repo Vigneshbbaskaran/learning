7c9d2ebff4abefccaec104d3b794290591d1c892
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-26 14:29:20 +0000

    Upload New File

---
+++ C-Basics/ARRAYS/9.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int i,temp,a[4],b;
+	printf("enter one digit  number\n");
+	scanf("%d",&b);
+	for(i=3;i>=0;i--)
+	{
+		temp=b%2;
+		b=b/2;
+		a[i]=temp;
+	}
+
+	printf("\n binary value: ");
+	for(i=0;i<4;i++)
+	{
+		printf("%d",a[i]);
+	}
+
+	printf("\n");
+	return 0;
+}
