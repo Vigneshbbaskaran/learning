7d807c853a34be0a88326aad3865338643a1ed28
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-26 13:53:49 +0000

    Replace 8.c

--- C-Basics/ARRAYS/8.c
+++ C-Basics/ARRAYS/8.c
@@ -1,21 +1,26 @@
-/*
-8)	"Write a program to reverse an array and print it:  
-Input : array1 ={1,2,3,4,5};
-Output : array1 = {5,4,3,2,1};"
-*/
 #include<stdio.h>
 int main()
 {
-	int a[5]={1,2,3,4,5};
-	int b[5],i,j=4;
+	int a[5];
+	int temp,i,j=5;
+	printf("enter 5 numbers to reverse\n");
 	for(i=0;i<5;i++)
 	{
-		b[j-i] = a[i];
+		scanf("%d",&a[i]);
 	}
-	 for(i=0;i<5;i++)
-        {
-                a[i] = b[i];
+		
+	for(i=0;i<5;i++)
+	{
+	j--;
+	if(i<j)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
 	}
+
+	}
+	printf("\n........REVERSED ARRAY..........\n");
 	 for(i=0;i<5;i++)
 	 {
 		 printf("\na[%d] = %d\n", i,a[i]);
