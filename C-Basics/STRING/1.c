afdb49bf9595511b28fcf60f4df1f907f2b8da15
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-26 17:05:38 +0000

    Upload New File

---
+++ C-Basics/STRING/1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int strlen (char st1[10]);
+int main()
+{
+	char st1[10]="hellow";
+	int a;
+	a=strlen(st1);
+
+	printf("\nstring length = %d\n", a);
+
+	return 0;
+}
+
+int strlen(char st1[10])
+{
+	int i;
+	for(i=0;st1[i]!='\0';i++);
+	return i;
+}
+
+
+
