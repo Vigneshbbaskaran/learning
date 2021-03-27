98aef16c14da0c11185027696e80a8a504b51e0a
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-27 08:44:14 +0000

    Upload New File

---
+++ C-Basics/STRING/5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void lower(char str[10]);
+int main()
+{
+	char str[10]="HeLLoW";
+	lower(str);
+
+	printf("lower = %s\n",str);
+	return 0;
+}
+
+void lower(char str[10])
+{
+	int i;
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]>=65 && str[i]<=90)
+			str[i] = str[i] + 32;
+	}
+	return ;
+}
