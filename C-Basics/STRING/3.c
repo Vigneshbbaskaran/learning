de4bea46d4c1d496e3d204f8efbb0905563a017f
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-27 07:26:38 +0000

    Upload New File

---
+++ C-Basics/STRING/3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void strcat1(char s1[20], char s2[20]);
+int main()
+{
+	char s1[20]= "hellow " , s2[20]= "world";
+	strcat1(s1,s2);
+
+	printf("concatenated s1=  %s\n",s1);
+	return 0;
+}
+
+void strcat1(char s1[20],char s2[20])
+{
+	int i,j;
+	for(i=0;s1[i]!='\0';i++);
+	j=i;
+	i=0;
+	for(;j<20;j++,i++)
+	{
+		s1[j]=s2[i];
+		if(s2[i]=='\0')
+			break;
+	}
+		
+return;
+}
+		
+
+
