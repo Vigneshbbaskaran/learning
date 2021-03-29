e96aefa9c99dc38cecd211953855733276751d72
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-29 02:06:38 +0000

    Upload New File

---
+++ C-Basics/pointers/1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	char a,b;
+	char *ptr1, *ptr2;
+
+	printf("enter a character:\n");
+	scanf("%c",&a);
+	printf("enter another character:\n");
+	scanf(" %c",&b);
+
+	ptr1= &a;
+	ptr2= &b;
+
+	if(*ptr1 > *ptr2)
+		printf("First char has highest ASCII value\n");
+	else if(*ptr1 < *ptr2)
+		printf("Second char has highest ASCII value\n");
+
+	return 0;
+}
+
+
+	
+
+
+
