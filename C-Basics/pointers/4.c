edf17f565d0d88a29e5a904e53230180b4e9d0ac
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-29 02:53:26 +0000

    Replace 4.c

--- C-Basics/pointers/4.c
+++ C-Basics/pointers/4.c
@@ -2,20 +2,21 @@
 int main()
 {
 	int i, a[10];
-	int *p1[10];
+	int *p1;
 	printf("ENTER VALUES\n");
 	for(i=0;i<10;i++)
 	{
 	printf("a[%d] = ",i);
 	scanf("%d",&a[i]);
-	p1[i]=&a[i];
-	}
 	
+	}
+	p1=&a[0];
 	
 	printf("\n\nENTERED VALUES\n");
 	for(i=0;i<10;i++)
 	{
-	printf("a[%d]:\nvalue = %d\nAddress = %d\n\n",i,*p1[i],p1[i]);
+	printf("a[%d]:\nvalue = %d\nAddress = %d\n\n",i,*p1,p1);
+	p1++;
 	}
 
 
