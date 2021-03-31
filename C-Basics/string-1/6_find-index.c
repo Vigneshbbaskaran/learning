ba0fb4f65ee0f199fb926a110e80e48dc2cc37ea
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-03-31 18:02:48 +0000

    Replace 6_find-index.c

--- C-Basics/string-1/6_find-index.c
+++ C-Basics/string-1/6_find-index.c
@@ -1,11 +1,5 @@
-/*
-6)Write a function called strstr that takes 2 strings as input, finds the first occurrence of second string, in the first string, and returns the index from where it starts in the first string. If the string is not found, returns -1.
-
-eg., Kernel Masters
-	Mast
-output : 7
-*/
 #include<stdio.h>
+int check(char a[100],char b[100]);
 int index(char a[100],char b[100]);
 int main()
 {
@@ -26,11 +20,23 @@ int main()
 }
 int index(char a[100],char b[100])
 {
-int i,temp=-1;
+int i,j,temp=-1,check=0;
 	for(i=0;a[i]!='\0';i++)
 	{		
 			if(a[i]==b[0])
-			{	return i;
+			{	
+				for(j=0;b[j]!='\0';j++)
+				{
+					if(a[i+j]==b[j])
+						check=1;
+					else
+					{	check=0;
+						break;
+					}
+				}
+				if(check==1)
+					return i;
+						
 			}
 			else
 				temp=-1;
