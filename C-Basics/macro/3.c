954b97728f30652c404d4acfa2af1968e1d81e39
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-04-02 14:59:47 +0000

    Upload New File

---
+++ C-Basics/macro/3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#define MAX(x,y) (x)>(y)?(x):(y)
+int main()
+{
+	int a,b,c,temp=0;
+	printf("\nenter a:");
+	scanf("%d",&a);
+	printf("\nenter b:");
+	scanf("%d",&b);
+	printf("\nenter c:");
+	scanf("%d",&c);
+	
+	temp=MAX(a,b);
+	if(temp==a)
+	{
+		temp=MAX(a,c);
+		if(temp==a)
+			 printf("a:%d is the largest number\n", a);
+		else
+		         printf("c:%d is the largest number\n", c);
+	}
+	else
+	{
+		temp=MAX(b,c);
+		if(temp==b)
+			 printf("b:%d is the largest number\n", b);
+		else
+		         printf("c:%d is the largest number\n", c);
+	}
+	
+	return 0;
+	}
