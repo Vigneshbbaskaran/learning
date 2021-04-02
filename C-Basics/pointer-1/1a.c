edb9783618f46b4a324365e3817a97b405f1f20e
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-04-02 04:38:41 +0000

    Upload New File

---
+++ C-Basics/pointer-1/1a.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void swap1(int a, int b);
+int main()
+{
+	int a,b;
+	printf("\nenter a: ");
+	scanf("%d",&a);
+	printf("\nenter b: ");
+	scanf("%d",&b);
+
+	swap1(a,b);
+
+	return 0;
+}
+
+
+void swap1(int a,int b)
+{
+	int temp=0;
+
+	temp=a;
+	a=b;
+	b=temp;
+
+	printf("\noutput:\na = %d\nb = %d\n",a,b);
+
+	return;
+}
+
