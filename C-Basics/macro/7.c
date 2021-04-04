d125c6f6ca7f800935ee654c09d874ab4a22dc85
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-04-04 13:03:00 +0000

    Upload New File

---
+++ C-Basics/macro/7.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+#define isprime(a)  		 { int temp=1,i=a;\
+        a--; \
+        for(;a>1;a--)\
+        {\
+                if(i%a==0)\
+                {\
+                        temp =0;\
+			break;\
+                }\
+        }\
+	return temp;\
+	}
+int check(int a)
+	isprime(a)
+	
+	
+int main()
+{
+int a,i,j;
+
+	printf("\nenter a number :\n");
+	scanf("%d",&a);
+	printf("prime numbers\n");
+	for(j=2;j<=a;j++)
+	{
+		if(check(j))
+			printf("%d\n",j);
+	}
+	
+	return 0;
+}
