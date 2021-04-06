356934ec9031f6b5feda66056b7cf5eade6b3efe
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-04-06 17:03:35 +0000

    Upload New File

---
+++ Embedded-c/9.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the number\n");
+	scanf("%d",&a);
+	int temp;
+	temp=a;
+
+	temp=temp<<3;
+	temp=temp+a;
+
+	printf("%d*9 : %d\n",a,temp);
+
+	return 0;
+}
+
+
