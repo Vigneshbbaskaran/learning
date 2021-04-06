f36004e675943c0574d0a64ba70cef3f1621f699
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-04-06 15:29:15 +0000

    Upload New File

---
+++ Embedded-c/4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int  main()
+{
+	int a,b;
+	printf("enter A: ");
+	scanf("%d",&a);
+	printf("\nenter B: ");
+	scanf("%d",&b);
+
+	
+
+	printf("\nA-B:%d\n",a+(~b+1));
+
+	return 0;
+}
+	
