a9132c6646628ade08c39bce93f41711ff03630c
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-04-07 02:15:38 +0000

    Upload New File

---
+++ Embedded-c/bitwise-operator/7_count-size.c
@@ -0,0 +1,18 @@
+/*7. WAP to implement the sizeof operation using the bitwise operator.
+*/
+#include<stdio.h>
+int main()
+{
+int x,count;
+	x=1;
+	while(x!=0)
+		{
+			x=x<<1;
+			count++;
+		}
+	printf("byte:%d\n",count/8);
+return 0;
+}
+	
+		
+		
