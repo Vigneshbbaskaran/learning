2077eb95cb6a76d53e3f78e7d3360e42c5f788fc
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-04-07 03:22:58 +0000

    Upload New File

---
+++ Embedded-c/bitwise-operator/15.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter the a:\n");
+	scanf("%d",&a);
+	printf("enter b:\n");
+	scanf("%d",&b);
+
+
+	a= a^b;
+	b= a^b;
+	a= a^b;
+
+	printf("swapped values:\na: %d\nb: %d\n",a,b);
+
+	return 0;
+}
+
+	
