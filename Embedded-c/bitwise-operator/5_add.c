3bafd8754436a81dd3eb3c1b4d140160f626343c
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-04-07 02:15:17 +0000

    Upload New File

---
+++ Embedded-c/bitwise-operator/5_add.c
@@ -0,0 +1,19 @@
+/*5. WAP implement addition functionality with out using ADD('+') Operator.  
+*/
+#include<stdio.h>
+
+int  main()
+{
+	int a,b;
+	printf("enter A: ");
+	scanf("%d",&a);
+	printf("\nenter B: ");
+	scanf("%d",&b);
+
+	
+
+	printf("\nA+B:%d\n",a-(~b+1));
+
+	return 0;
+}
+	
