31090b5b9956dc6a453b0f047ce1f180a8c6758c
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-04-08 17:25:49 +0000

    Update 14.c

--- Embedded-c/bitwise-operator/14.c
+++ Embedded-c/bitwise-operator/14.c
@@ -1,3 +1,13 @@
+/*14. Write a functionsetbits(x,p,n,y)that return x with then bits that begin at position p set to the rightmost n bits of y, leaving the other bits unchanged.
+e.g. if x = 10101010 (170 decimal) and y = 10100111 (167 decimal) and n = 3 and p = 6 then
+you need to strip off 3 bits of y (111) and put them in x at position 10xxx010 to get answer
+10111010. Your answer should print out the result in binary form although input can be in
+decimal form.
+Your output should be like this:
+x = 10101010 (binary)
+y = 10100111 (binary)
+setbits n = 3, p = 6 gives x = 10111010 (binary).*/
+
 #include<stdio.h>
 
 void print_binary(int temp)
