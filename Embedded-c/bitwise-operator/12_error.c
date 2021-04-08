b5a79f5fc9a1d6b8c879db838fd5f07e3f83d241
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-04-08 05:11:23 +0000

    Replace 12_error.c

--- Embedded-c/bitwise-operator/12_error.c
+++ Embedded-c/bitwise-operator/12_error.c
@@ -5,20 +5,42 @@ int main()
 	int a,i,b=0,temp;
 	printf("enter value\n");
 	scanf("%d",&a);
+	printf("entered binary value\n");
+	for(i=0;i<32;i++)
+	{
+		if(a & (0x01<<31))
+			printf("1");
+
+		else
+			printf("0");
+		a=a<<1;
+	}
 
 	for(i=0;i<32;i++)  // Perform operation
 	{
 		if( (a & (0x01<<31) ) == (0x01<<31) )
 		{
-		 (b | (0x01<<31));
+		 b=(b | (0x01<<31));
 		printf("1");
 		}
-		b>>1;
-		a<<1;	
+		b=b>>1;
+		a=a<<1;	
 		
 	}
+	
+	printf("\nafter reverse\n");
+	for(i=0;i<32;i++)
+	{
+		if(a & (0x01<<31))
+			printf("1");
+
+		else
+			printf("0");
+		a=a<<1;
+	}
+
 
 	printf("\n");
-	printf("\na:%d\n",b);
+	printf("\nb:%d\n",b);
 	return 0;
 }
