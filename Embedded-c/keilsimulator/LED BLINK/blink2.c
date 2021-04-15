fa8d724cd71157b59567b9dc765f033b12def1eb
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-04-15 15:00:36 +0000

    Upload New File

---
+++ Embedded-c/keilsimulator/LED BLINK/blink2.c
@@ -0,0 +1,88 @@
+#define RCC_AHB1ENR *((int *)(0X40023830))
+#define GPIOC_MODER  *((int *)(0X40020800))
+#define GPIOA_MODER  *((int *)(0X40020000))
+#define GPIOC_ODR    *((int *)(0X40020814))
+#define GPIOA_ODR    *((int *)(0X40020014))
+void RED_LED_INT(void)
+{
+	
+	RCC_AHB1ENR |=(0x5); // enable PORT C and PORT A
+	while(!(RCC_AHB1ENR & 0x5))
+	{
+		;
+	}
+	GPIOC_MODER &= ~(0X3<<10); // CLEAR
+	GPIOA_MODER &= ~(0X3<<16); // CLEAR
+	GPIOC_MODER |= (0x01)<<10; // ENABLE OUTPUT MODE
+	GPIOA_MODER |= (0x01)<<16; // ENABLE OUTPUT MODE
+}
+
+void DELAY(int a)
+{	int i;
+	for(i=0;i<(1700*a);i++)
+	{
+		;
+	}
+}
+int main()
+{
+RED_LED_INT();
+
+
+	while(1)
+	{	
+		GPIOA_ODR |= (0X01<<8); //SET PA8
+		GPIOC_ODR |= (0X01<<5); //SET PC5
+		DELAY(50);
+		
+		GPIOA_ODR &= ~(0X01<<8); //CLEAR PA8
+		DELAY(50);
+		
+		GPIOA_ODR |= (0X01<<8); //SET  PA8
+		DELAY(50);
+		
+		GPIOA_ODR &= ~(0X01<<8); //CLEAR PA8
+		GPIOC_ODR &= ~(0X01<<5); //CLEAR PC5
+		DELAY(150);
+	//	-------------------------------------------------------- 
+		GPIOA_ODR |= (0X01<<8); //SET PA8
+		GPIOC_ODR |= (0X01<<5); //SET PC5
+		DELAY(50);
+		
+		GPIOA_ODR &= ~(0X01<<8); //CLEAR PA8
+		GPIOC_ODR &= ~(0X01<<5); //CLEAR PC8
+		DELAY(50);
+	//------------------------------------------------ 450
+		GPIOA_ODR |= (0X01<<8); //SET PA8
+		DELAY(50);
+		
+		GPIOA_ODR &= ~(0X01<<8); //CLEAR PA8		
+		DELAY(50);
+		
+		GPIOA_ODR |= (0X01<<8); //SET PA8
+		DELAY(50);
+		
+		GPIOA_ODR &= ~(0X01<<8); //CLEAR PA8		
+		DELAY(150);
+		
+		GPIOA_ODR |= (0X01<<8); //SET PA8
+		DELAY(50);
+		
+		GPIOA_ODR &= ~(0X01<<8); //CLEAR PA8		
+		DELAY(50);
+		
+		GPIOA_ODR |= (0X01<<8); //SET PA8
+		DELAY(50);
+		
+		GPIOA_ODR &= ~(0X01<<8); //CLEAR PA8		
+		GPIOC_ODR &= ~(0X01<<5); //CLEAR PC8
+		DELAY(50);
+	//-----------------------------------
+		
+	
+		
+	}
+
+	
+
+}
