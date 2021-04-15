1bc09da6cdb0c3bcde5e7b1e7feb9becec0afcfa
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-04-15 14:59:19 +0000

    Upload New File

---
+++ Embedded-c/keilsimulator/LED BLINK/blink1.c
@@ -0,0 +1,39 @@
+#define RCC_AHB1ENR *((int *)(0X40023830))
+#define GPIOC_MODER  *((int *)(0X40020800))
+#define GPIOC_ODR    *((int *)(0X40020814))
+
+void RED_LED_INT(void)
+{
+	
+	RCC_AHB1ENR |=(0x1<<2); // enable PORT C
+	while(!(RCC_AHB1ENR & 0x4))
+	{
+		;
+	}
+	GPIOC_MODER |= (0x01)<<10; //enable output mode for PORT C
+}
+
+void DELAY(int a)
+{	int i;
+	for(i=0;i<(1700*a);i++)
+	{
+		;
+	}
+}
+int main()
+{
+RED_LED_INT(); //initilization
+
+	
+	while(1)
+	{
+		GPIOC_ODR &= ~(0X01<<5);  //clear
+		DELAY(50);
+		GPIOC_ODR |= (0X01<<5);   //set
+		DELAY(1300);
+		
+	}
+
+	
+
+}
