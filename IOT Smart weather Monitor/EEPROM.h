5469b7af5cca2be3d47ebcc982f3f0a66de4df25
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-05-10 16:48:31 +0000

    Upload New File

---
+++ IOT Smart weather Monitor/EEPROM.h
@@ -0,0 +1,9 @@
+void EEPROM_SW_INT();
+void I2C1_EEPROM_Read();
+	
+void I2C1_EEPROM_Configuration(void);
+void I2C1_EEPROM_write(int address);
+	
+void I2C1_EEPROM_Initilization(void);
+
+int temp_2_EEPROM();
