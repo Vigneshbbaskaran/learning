b9d0b0da5066822a5a316f193177520f410c6c72
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-06-14 07:33:16 +0000

    Upload New File

---
+++ Advance-C/Strings Pointer Argument/4.c
@@ -0,0 +1,77 @@
+4)WAP to display the word expansion of a number
+eg., Input : 789
+Output : Seven Eight Nine
+*/
+#include<stdio.h>
+void expansion(int a);
+int main()
+{
+	int a;
+	printf("enter a number\n");
+	scanf("%d",&a);
+
+	expansion(a);
+
+	printf("\n");
+
+	return 0;
+}
+
+void expansion(int b)
+{
+	int i=0,j=1,temp;
+	char a[100];
+	for(i=0;i<100;i++)
+	{
+	if(b==0)
+		break;
+
+		a[i]=b%10;
+		b=b/10;
+	}
+	j=i;	
+	int x=0;
+	for(;i!=0;i--)
+	{	
+		if(x!=0)
+			printf(" ");
+		j--;
+		x++;
+		temp=a[j];
+		switch(temp)
+		{
+			case 0:
+				printf("zero");
+				break;
+			case 1:
+                                printf("one");
+                                break;
+			case 2:
+                                printf("two");
+                                break;
+			case 3:
+                                printf("three");
+                                break;
+			case 4:
+                                printf("four");
+                                break;
+			case 5:
+                                printf("five");
+                                break;
+			case 6:
+                                printf("six");
+                                break;
+			case 7:
+                                printf("seven");
+                                break;
+			case 8:
+                                printf("eight");
+                                break;
+			case 9:
+                                printf("nine");
+                                break;
+		}
+	}
+}
+
+
