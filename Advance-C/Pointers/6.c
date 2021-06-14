5b07b106557c9379c904a58ce43bea92c4aaa5cf
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-06-14 04:25:15 +0000

    Deleted Advance-C/Pointers/1.c, Advance-C/Pointers/2.c, Advance-C/Pointers/3.c, Advance-C/Pointers/4.c, Advance-C/Pointers/5.c, Advance-C/Pointers/6.c, Advance-C/Pointers/7.c, Advance-C/Pointers/8.c, Advance-C/Pointers/9.c files

--- Advance-C/Pointers/6.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int *int1;
-	char *char1;
-	float *float1;
-
-	printf("size\nSize of int pointer=%ld\nSize of char pointer=%ld\nSize of float pointer=%ld\n",sizeof(int1),sizeof(char1),sizeof(float1));
-}
