1d77bb3b3fc7e7981591f362483e6980fec5fcbd
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-06-14 09:36:06 +0000

    Upload New File

---
+++ Advance-C/Strings Pointer Argument/13.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int cmp(char a[10],char b[10]);
+int main()
+{
+	char a[10];
+	char b[10];
+	printf("\nenter a string1:  ");
+	scanf("%9s",a);
+	printf("\nenter string2:  ");
+	scanf("%9s",b);
+	int temp;
+	temp = cmp(a,b);
+
+	printf("\n%d\n",temp);
+
+	return 0;
+}
+int cmp (char *a,char *b)
+{
+	int i;
+	for(i=0;(*(a+i)!='\0')||(*(b+i)!='\0');i++)
+	{
+		if((*(a+i))>(*(b+i)))
+			return 1;
+		if((*(a+i))<(*(b+i)))
+			return -1;
+		
+	}
+	return 0;
+}
+
