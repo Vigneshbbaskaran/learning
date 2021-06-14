f13be4169cd487b0858acc8776a090b3fb465923
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-06-14 04:19:07 +0000

    Update 8.c

--- Advance-C/8.c
+++ Advance-C/8.c
@@ -1,3 +1,8 @@
+//8. Take three input integers x,y and z. 
+//Write a program to rotate their values such that, x has the value of y, y has the value of z and z has the value of x. 
+//Do this using pointers that point to x,y and z.
+
+
 #include<stdio.h>
 int main()
 {
