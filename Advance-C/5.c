be59fde716391cbaa148d8b45f7202b576a0ae6e
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-06-14 04:17:00 +0000

    Update 5.c

--- Advance-C/5.c
+++ Advance-C/5.c
@@ -1,3 +1,11 @@
+//5. Declare 3 pointer variables of integer type, and an integer variable. 
+//Assign the address of integer variable to the 3 pointers. 
+//Print the variable value using indirection on each pointer variable. 
+//Change the variable value directly and check the values of each of the pointers using indirection.
+//Change the variable value , using each pointer and print the variable value and indirected values of all pointers every time you change.
+
+
+
 #include<stdio.h>
 int main()
 {
