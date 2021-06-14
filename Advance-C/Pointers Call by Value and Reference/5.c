39eaca5321581d565274e301e46465436d43764a
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-06-14 05:57:42 +0000

    Update 5.c

--- Advance-C/Pointers Call by Value and Reference/5.c
+++ Advance-C/Pointers Call by Value and Reference/5.c
@@ -1,3 +1,19 @@
+/*
+5. Write a function that communicates with main using a single static variable without taking any input arguments.
+Everytime function returns something using the static variable,after using it, main sends another input using the same variable and calls the function again.
+
+eg., print the square of each number of an array :
+for each number of the array :
+    call the function
+    main gets the static variable address as return value from function.
+    main puts the array element in static variable.
+in the function :
+    create static variable.
+    if static variable value is not zero, print its square.
+    function sends static variable address back to main.
+*/
+
+
 #include<stdio.h>
 int* square(void);
 void main()
