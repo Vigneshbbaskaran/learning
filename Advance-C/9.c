b7a32937e83dcfbb88ba0d93ccffaae78dc9a76f
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-06-14 04:19:50 +0000

    Update 9.c

--- Advance-C/9.c
+++ Advance-C/9.c
@@ -1,3 +1,6 @@
+//9. Declare an integer array of size 10 and initialize it to some values. 
+//Print the addresses of each element of the array using a pointer. using indirection operator , print the value stored in each element of the array.
+
 #include<stdio.h>
 int main()
 {
