cda082b6cb7486a0840c5b268c3ccde26c30f339
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-06-14 07:34:29 +0000

    Upload New File

---
+++ Advance-C/Strings Pointer Argument/6.c
@@ -0,0 +1,52 @@
+/*
+6)Write a function called strstr that takes 2 strings as input, finds the first occurrence of second string, in the first string, and returns the index from where it starts in the first string. If the string is not found, returns -1.
+
+eg., Kernel Masters
+	Mast
+output : 7
+*/
+#include<stdio.h>
+int index(char *a,char *b);
+int main()
+{
+	char a[100],b[100];
+
+	printf("enter a string:\n");
+	scanf("%[^\n]s",a);
+
+	printf("enter another string for search\n");
+	scanf("%s",b);
+	int temp;
+	temp = index(a,b);
+	if(temp>=0)
+		printf("\nfound at %d\n",temp);
+	else 
+		printf("\nNot found\n");
+	return 0;
+}
+int index(char *a,char *b)
+{
+int i,j,temp=-1,check=0;
+	for(i=0;*(a+i)!='\0';i++)
+	{		
+			if(*(a+i)==*b)
+			{	
+				for(j=0;*(b+j)!='\0';j++)
+				{
+					if(*(a+i+j)==*(b+j))
+						check=1;
+					else
+					{	check=0;
+						break;
+					}
+				}
+				if(check==1)
+					return i;
+						
+			}
+			else
+				temp=-1;
+	}
+	return temp;
+}
+
