7e17ed4fc100039dcce0bcc18b6f2cfeb390ed87
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-06-14 09:31:47 +0000

    Upload New File

---
+++ Advance-C/Strings Pointer Argument/9.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void strcat1(char s1[20], char s2[20]);
+int main()
+{
+	char s1[20]= "hellow " , s2[20]= "world";
+	strcat1(s1,s2);
+
+	printf("concatenated s1=  %s\n",s1);
+	return 0;
+}
+
+void strcat1(char *s1,char *s2)
+{
+	int i,j;
+	for(i=0;*(s1+i)!='\0';i++);
+	j=i;
+	i=0;
+	for(;*(s2+i)!='\0';j++,i++)
+	{
+		s1[j]=s2[i];
+	}
+		
+return;
+}
+
