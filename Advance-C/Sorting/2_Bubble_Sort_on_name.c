53282d93d300227c22803a5658f838271a46bd1e
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-06-24 19:36:12 +0000

    Replace 2_Bubble_Sort_on_name.c

--- Advance-C/Sorting/2_Bubble_Sort_on_name.c
+++ Advance-C/Sorting/2_Bubble_Sort_on_name.c
@@ -1,34 +1,45 @@
-//2. Perform Bubble Sort on names.
 #include<stdio.h>
 #include<string.h>
+void swap1(char **p1,char **p2)
+{
+	char *a=NULL;
+
+	a=*p1;
+	*p1=*p2;
+	*p2=a;
+
+}
 void main()
 {
 	int i,j,k=0;
-	char *a;
-	char a[4][10];
-	char *p;
-	p=a;
+	char *a=NULL;
+	char name[][10]={"heelllow","null","boo","second"};
+	char *p[4]={NULL,NULL,NULL,NULL};
+	for(i=0;i<4;i++)
+		p[i]=&name[i][0];
+	
+	
 	for(i=0;i<4;i++)
 	{
-		printf("enter your name\n");
-		scanf("\n%s",a[i]);
+		printf("enter your name  :");
+		scanf("\n%s",name[i]);
 	}
 	for(i=0;i<4;i++)
 	{			
+
 			k=0;
 			for(j=0;j<4-1;j++)
 			{
 				if((strcmp(p[j],p[j+1]))>0)
 				{
 				k=1;
-				a=p[j];
-				p[j]=p[j+1];
-				p[j+1]=a;
-				}
+				swap1(&p[j],&p[j+1]);
+				}			
 			}
 			if(k==0)
 			break;		
-	}	
-		for(i=1;i<4;i++)
-		printf("%s\n",p[i]);
+	}
+	printf("\nSorted Names:\n");	
+		for(i=0;i<4;i++)
+		printf("\t\t%s\n",p[i]);
 }
