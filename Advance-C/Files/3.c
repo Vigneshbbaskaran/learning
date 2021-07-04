e9373bb2863ecc307d5c3e43898b977b6f9a0f5e
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-07-04 23:00:30 +0000

    Upload New File

---
+++ Advance-C/Files/3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int i,a;
+	FILE *f,*f1;
+	char line;
+	f=fopen("1","r");
+	f1=fopen("2","w");
+
+	while(1)
+	{	
+		line=getc(f);
+                if (feof(f))
+                         break;
+                fprintf(f1,"%c",line);
+	}
+	fclose(f);
+	fclose(f1);
+	f=fopen("2","r");
+	 while (1)
+        {       line=getc(f);
+                if (feof(f))
+                	 break;
+		printf ("%c",line);
+        }
+	fclose(f);
+}
+
