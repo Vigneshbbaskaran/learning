96cb2d56460f1cbbb0573da3ca2b97fb0d04c8bf
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-07-04 22:45:23 +0000

    Upload New File

---
+++ Advance-C/Files/1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int i,a;
+	FILE *f;
+	char line;
+	f=fopen("1","w");
+	while(1)
+	{
+		printf("ENTER a number :");
+		scanf("%d",&a);
+		if(a<0)
+			break;
+		fprintf(f,"%d\n",a);
+	}
+	fclose(f);
+	f=fopen("1","r");
+	while (1) 
+	{	line=getc(f);
+		if (feof(f))
+ 	           break;
+ 		printf ("%c",line);
+	}
+	fclose(f);
+}
+
