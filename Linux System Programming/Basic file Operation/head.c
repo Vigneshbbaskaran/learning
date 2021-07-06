1e8d1be3aa5fb114e29340be082d562293e42e28
Vignesh Bhaskaran <vigneshbbaskaran@gmail.com>
2021-07-06 12:42:11 +0000

    Upload New File

---
+++ Linux System Programming/Basic file Operation/head.c
@@ -0,0 +1,38 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[])
+{	
+	char buff;
+	int fd,i,j,count=0;
+	for(i=1;i<argc;i++)
+	{
+		fd=open(argv[i],O_RDONLY);
+		if(fd<0)
+		{
+			perror("Open fail");
+			_exit(1);
+		}
+		while(1)
+		{
+			j=read(fd,&buff,1);
+			if(j<0)
+			{
+				perror("Read fails..!!");
+				_exit(1);
+			}
+			if(j==0 |count==10 )
+				break;
+			write(1,&buff,1);
+			if(buff=='\n')
+				count++;
+
+		}
+		close(fd);
+	}
+	return 0;
+}
