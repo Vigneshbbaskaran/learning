de82c2132cb1ed0a68d1c90b8982204da2f83ae6
Vigneshbbaskaran <89230718+Vigneshbbaskaran@users.noreply.github.com>
2022-05-19 12:21:25 +0530

    Delete IOT Smart weather Monitor directory

--- IOT Smart weather Monitor/EEPROM.h
+++
@@ -1,9 +0,0 @@
-void EEPROM_SW_INT();
-void I2C1_EEPROM_Read();
-	
-void I2C1_EEPROM_Configuration(void);
-void I2C1_EEPROM_write(int address);
-	
-void I2C1_EEPROM_Initilization(void);
-
-int temp_2_EEPROM();
